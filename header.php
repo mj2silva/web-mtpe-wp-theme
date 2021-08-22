849f2fa992b0f3c0c3614ee70089800a1ecf7851
ricardoserin <serin.ricardo@gmail.com>
2021-08-22 18:49:02 -0500

    datepicker

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/datepicker/css/bootstrap-datepicker.standalone.css">
   <?php wp_head() ?>
 </head>
 
