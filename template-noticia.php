114b3fad93bc6e71c440ab78260569c76939c290
ricardoserin <serin.ricardo@gmail.com>
2021-08-23 10:30:32 -0500

    Formulario de filtros responsive

--- template-noticia.php
+++ template-noticia.php
@@ -14,62 +14,80 @@
   </script>
   <div class="row">
     <div class="col-md-4">
-      <aside class="my-5 utils no-print social-utils">
+      <aside class="my-2 utils no-print social-utils">
         <h1>Noticias</h1>
-        <div class="">
-          <form action="">
-            <div class="container py-4">
-              <div class="form-group">
-                <label class="fw-bold mb-2" for="">Filtros de búsqueda</label>
-                <input class="input" type="text" placeholder="Palabra clave">
-              </div>
-            </div>
-            <div class="container-fluid container--gray py-4">
-              <div class="input-daterange form-group pb-4" id="datepicker">
-                <label class="fw-bold mb-2" for="">Fecha de publicación</label>
-                <div class="row">
-                  <div class="col-5">
-                    <input type="text" class="form-control input" placeholder="Desde" name="start" />
-                  </div>
-                  <div class="col-1">
-                    <span class="input-group-addon">-</span>
-                  </div>
-                  <div class="col-5">
-                    <input type="text" class="form-control input" placeholder="Hasta" name="end" />
-                  </div>
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button class="accordion__button" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <span class="dashicons dashicons-filter"></span>
+                Filtros de búsqueda
+              </button>
+            </h2>
+            <!-- <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+            </div> -->
+          </div>
+        </div>
+        <div id="flush-collapseOne" class="accordion-collapse collapse accordion-responsive" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <form action="">
+              <div class="container">
+                <div class="form-group pb-4">
+                  <label class="fw-bold mb-2" for="">Filtros de búsqueda</label>
+                  <input class="input" type="text" placeholder="Palabra clave">
                 </div>
               </div>
-              <div class="form-group">
-                <label class="fw-bold mb-2" for="">Categorías</label>
-                  <?php
-                    if(count($categorias) > 0) 
-                    {
-                      ?>
-                        <select class="select" aria-placeholder="Tipo de categorías" name="" id="">
-                          <option hidden selected>Tipo de categorías</option>
-                          <?php 
-                            foreach ($categorias as $categoria) {
-                              ?>
-                                <option value="">
-                                  <?php echo $categoria->name  ?>
-                                </option>
-                              <?php
-                            }
-                          ?>
-                        </select>
-                      <?php
-                    }
-                    else {
-                      ?>
-                        <select disabled class="select" aria-placeholder="Tipo de categorías" name="" id="">
-                          <option hidden selected>No disponible</option>
-                        </select>
-                      <?php
-                    }
-                  ?>
+              <div class="container-fluid container--gray py-4">
+                <div class="input-daterange form-group pb-4" id="datepicker">
+                  <label class="fw-bold mb-2" for="">Fecha de publicación</label>
+                  <div class="row">
+                    <div class="col-5">
+                      <input type="text" class="form-control input" placeholder="Desde" name="start" />
+                    </div>
+                    <div class="col-1">
+                      <span class="input-group-addon">-</span>
+                    </div>
+                    <div class="col-5">
+                      <input type="text" class="form-control input" placeholder="Hasta" name="end" />
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="fw-bold mb-2" for="">Categorías</label>
+                    <?php
+                      if(count($categorias) > 0) 
+                      {
+                        ?>
+                          <select class="select" aria-placeholder="Tipo de categorías" name="" id="">
+                            <option hidden selected>Tipo de categorías</option>
+                            <?php 
+                              foreach ($categorias as $categoria) {
+                                ?>
+                                  <option value="">
+                                    <?php echo $categoria->name  ?>
+                                  </option>
+                                <?php
+                              }
+                            ?>
+                          </select>
+                        <?php
+                      }
+                      else {
+                        ?>
+                          <select disabled class="select" aria-placeholder="Tipo de categorías" name="" id="">
+                            <option hidden selected>No disponible</option>
+                          </select>
+                        <?php
+                      }
+                    ?>
+                </div>
               </div>
-            </div>
-          </form>
+              <div class="container py-3">
+                <input class="button button--primary button--full" type="submit" value="Mostrar resultados">
+              </div>
+            </form>
+          </div>
         </div>
       </aside>
     </div>
