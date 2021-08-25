001758a75744b3c2840af60dae14d854274a57df
Manuel Silva <manuel.silvag1@gmail.com>
2021-08-25 02:20:44 -0500

    Bloques colapsables de estilo 2 que acepta titulo y contenido

--- blocks/src/components/Collapse1/Collapse1Save.jsx
+++ blocks/src/components/Collapse1/Collapse1Save.jsx
@@ -1,13 +1,12 @@
 
-import { InnerBlocks } from '@wordpress/block-editor';
-// import { TextControl } from '@wordpress/components';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 const Collapse1Save = (props) => {
   const { attributes: { title, description } } = props;
   const rand = ((new Date().getTime()) * Math.floor(Math.random() * 1000)).toString(16);
   return (
     <div class="container">
-      <div class="row">
+      <div class="row g-0">
         <div class="col-9">
           <h3 class="subtitle">{title}</h3>
           <p>{description}</p>
@@ -15,8 +14,7 @@ const Collapse1Save = (props) => {
         <div class="col-3 d-flex align-items-center">
           <button class="button button--tertiary" type="button" data-bs-toggle="collapse" data-bs-target={"#collapsable" + rand}
             aria-expanded="false" aria-controls="collapsable">
-            <span class="button__text">Ver todos</span>
-            {' '}
+            <span class="d-none d-md-inline button__text">Ver todos{'  '}</span>
             <span class="material-icons button__icon button__icon--transform">arrow_back_ios_new</span>
           </button>
         </div>
---
+++ blocks/src/components/Collapse2/Collapse2Save.jsx
@@ -0,0 +1,33 @@
+
+import { InnerBlocks } from '@wordpress/block-editor';
+
+const Collapse2Save = (props) => {
+  const { attributes: { title, icon, id } } = props;
+  return (
+    <div className="container collapsable">
+      <div className="row my-2 d-flex justify-content-between">
+        <div className="col-10 p-0 d-flex align-items-center">
+          {
+            icon && (
+              <span className="d-inline-block collapsable__icon">
+                <img src={icon.url} alt={icon.alt} />
+              </span>
+            )
+          }
+          <h4 className="collapsable__title">{title}</h4>
+        </div>
+        <div className="col-1 d-flex align-items-center justify-content-end">
+          <button className="button collapsable__toggle" type="button" data-bs-toggle="collapse"
+            data-bs-target={"#collapsable2" + id} aria-expanded="true" aria-controls="collapsable">
+            <span className="material-icons button__icon button__icon--transform">arrow_back_ios_new</span>
+          </button>
+        </div>
+      </div>
+      <div className="row collapse collapsable__content g-0" id={"collapsable2" + id}>
+        <InnerBlocks.Content />
+      </div>
+    </div>
+  );
+}
+
+export default Collapse2Save;
