ec8ba4698dd01ca6dbfd3289cbb1a62f8157fcb8
Manuel Silva <manuel.silvag1@gmail.com>
2021-12-22 19:16:51 -0500

    Solucion boton de collapsables 1

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e5d44e30f360eeca34b88742591adaec');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '322cb8b63aceabad212eb7c8f8dcb543');
--- blocks/src/components/Collapse1/Collapse1Save.jsx
+++ blocks/src/components/Collapse1/Collapse1Save.jsx
@@ -4,20 +4,20 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 const Collapse1Save = (props) => {
   const { attributes: { title, description, id } } = props;
   return (
-    <div class="container px-0 collapsable2 accordion" id={"accordion"+id}>
+    <div class="container px-0 collapsable2 accordion" id={"accordion3"+id}>
       <div class="accordion-item border-0">
-        <div class="accordion-header row g-0" id={"collapsablehead" + id}>
+        <div class="accordion-header row g-0" id={"collapsable3head" + id}>
           <div class="col-9">
             <h3 class="subtitle">{title}</h3>
             <p>{description}</p>
           </div>
           <div class="col-3 d-flex align-items-center justify-content-center justify-content-lg-end">
-            <button class="button button--tertiary accordion-button button--dflex justify-content-center w-75 collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapsable" + id} aria-expanded="false" aria-controls={"collapsable" + id}>
+            <button class="button button--tertiary accordion-button button--dflex justify-content-center w-75 collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapsable3" + id} aria-expanded="false" aria-controls={"collapsable3" + id}>
               <span class="d-none d-lg-inline button__text">Ver todos</span>
             </button>
           </div>
         </div>
-        <div class="row collapse" id={"collapsable" + id} aria-labelledby={"collapsablehead" + id} data-bs-parent={"accordion"+id}>
+        <div class="row collapse" id={"collapsable3" + id} aria-labelledby={"collapsable3head" + id}>
           <div className="accordion-body">
             <InnerBlocks.Content />
           </div>
